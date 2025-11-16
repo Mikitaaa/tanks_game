33ec5c5d04cae6173bc1bf761388366731007587
Lisskaaaa <liska.loveiska147@gmail.com>
2025-11-16 18:18:50 +0100

    update  creating field and printField

--- field.cpp
+++ field.cpp
@@ -5,6 +5,19 @@ using namespace std;
 
 Field::Field(int w, int h) : width(w), height(h) {
     grid = std::vector<std::vector<int>>(h, std::vector<int>(w, 0));
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+                grid[i][j] = 1;
+            }
+            else {
+                grid[i][j] = 0;
+            }
+        cout << endl;
+        }
+
+    }
 }
 
 int Field::getWidth() const {
@@ -17,16 +30,11 @@ int& Field::at(int width, int height) {
     return grid[width][height];
 }
 void Field::printField(){
-    cout << string(width, '*') << endl;
-    for (int i = 0; i < height - 2; i++){
-        for (int j = 0; j < width; j++){
-            if (j == 0 or j == width - 1)
-                cout << "*";
-            else {
-                cout << grid[i][j];
-            }
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            cout << grid[i][j];
         }
         cout << endl;
     }
-     cout << string(width, '*') << endl;
 }
+
